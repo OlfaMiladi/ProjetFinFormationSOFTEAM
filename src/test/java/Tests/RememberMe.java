b1a3c2fb732eb6e311c40ff026ecd8999f999eb9
Nour-Eddine <noureddinelarabi@gmail.com>
2023-01-16 10:44:02 +0100

    telecharger le travail des autres

--- src/test/java/Tests/RememberMe.java
+++ src/test/java/Tests/RememberMe.java
@@ -1,13 +1,6 @@
 package Tests;
 
-import pages.MyAccountPage;
-
 public class RememberMe extends ConnexionOk {
 
-    public void FonctionRememberMe(){
-        MyAccountPage CaseRemembreme = new MyAccountPage(driver);
-        CaseRemembreme.FonctionRememberMe();
 
-
-    }
 }
