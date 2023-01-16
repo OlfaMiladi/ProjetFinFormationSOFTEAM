976f32b74ae636316a04fa9dc205f449a6e0d979
Nour-Eddine <noureddinelarabi@gmail.com>
2023-01-16 11:07:01 +0100

    version

--- src/test/java/Tests/RememberMe.java
+++ src/test/java/Tests/RememberMe.java
@@ -1,6 +1,16 @@
 package Tests;
 
+import org.testng.annotations.Test;
+
 public class RememberMe extends ConnexionOk {
+
+    @Test
+    public void  FonctionRememberme (){
+
+        Pages.MyAccountPage RememberMe = new Pages.MyAccountPage(driver);
+        RememberMe.FonctionRememberMe();
+
+    }
 
 
 }
