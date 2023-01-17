8043a7e6bee93da3402d276a6512dd2135c1da66
Nour-Eddine <noureddinelarabi@gmail.com>
2023-01-17 23:04:35 +0100

    Correction des bugs

--- src/main/java/Pages/MyAccountPage.java
+++ src/main/java/Pages/MyAccountPage.java
@@ -60,8 +60,9 @@ public class MyAccountPage {
     public void VerifMsgError() {
         WebElement verfError = driver.findElement(By.className("woocommerce-error"));
         String MsgError = verfError.getText();
-        //System.out.println(MsgError);
+
         Assert.assertTrue(MsgError.contains("Error: the password you entered for the username "), "Le message n'est pas affiché");
+    }
 
         /*
         if (MsgError.equals("Error: the password you entered for the username automaticiens@yopmail.com is incorrect. Lost your password?")) {
@@ -70,10 +71,9 @@ public class MyAccountPage {
         } else {
             System.out.println("Le text n'est pas affiché correctement");
         }
-
+                //System.out.println(MsgError);
          */
 
-    }
 
     //Le bon fonctionnement de la case à cocher REMEMBER ME
 
@@ -116,37 +116,36 @@ public class MyAccountPage {
         String Register = PaveRegister.getText();
         Assert.assertTrue(Register.contains("Register"), "Le Pavé Login n'est pas disponible");
     }
-/*
+
+
     //Reinitialisation de mot de passe
-    public void ReinitialiserMDP (){
-        WebElement ReinitiaMDP= driver.findElement(By.xpath("//*[@id=\"customer_login\"]/div[1]/form/p[4]/a"));
+    public void ReinitialiserMDP() {
+        WebElement ReinitiaMDP = driver.findElement(By.xpath("//*[@id=\"customer_login\"]/div[1]/form/p[4]/a"));
         ReinitiaMDP.click();
 
     }
-    public void SaisirmailReinit(String Val2){
+
+    public void SaisirmailReinit(String Val2) {
         WebElement ReinitMAil = driver.findElement(By.id("user_login"));
         ReinitMAil.sendKeys(Val2);
     }
-    public void ResetPasseword(){
+
+    public void ResetPasseword() {
         WebElement ResetMDP = driver.findElement(By.className("woocommerce-Button button"));
-       ResetMDP.click();
-    }*/
+        ResetMDP.click();
+    }
 
 
     //Vérification de la présence des pages : Dashboard, Orders, Download, Adresses, Account Details, Logout
 
-<<<<<<< HEAD
-    public void VerifDashboardPage(){
-        WebElement Dashboard= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[1]/a"));
-        String DBoard= Dashboard.getText();
-        Assert.assertTrue(DBoard.contains("Dashboard"), "La page Dashboard n'existe pas" );
-=======
+
     public void VerifDashboardPage() {
         WebElement Dashboard = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[1]/a"));
         String DBoard = Dashboard.getText();
         Assert.assertTrue(DBoard.contains("Dashboard"), "La page Dashboard n'existe pas");
->>>>>>> 20be10897d15e09199badeafc9eb4259eef5f867
     }
+
+
 
     public void DashboardPage() {
         WebElement Dashboard = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[1]/a"));
@@ -185,29 +184,20 @@ public class MyAccountPage {
         WebElement Adresses = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[4]/a"));
         Adresses.click();
     }
-<<<<<<< HEAD
-    public void VerifAccountDetailsPage(){
-        WebElement AccountDetails= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[5]/a"));
-        String AccDetails= AccountDetails.getText();
-        Assert.assertTrue(AccDetails.contains("Account Details"), "La page Account Details n'existe pas" );
-    }
-    public void AccountDetailsPage(){
-        WebElement AccountDetails= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[5]/a"));
-        AccountDetails.click();
-    public void AccountDetailsPage(){
-        WebElement AccountDetails= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[5]/a"));
-        AccountDetails.click();
-       /*String AccDetails= AccountDetails.getText();
-        Assert.assertTrue(AccDetails.contains("Account Details"), "La page Account Details n'existe pas" );*/
-           }
-=======
->>>>>>> 20be10897d15e09199badeafc9eb4259eef5f867
 
     public void VerifAccountDetailsPage() {
         WebElement AccountDetails = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[5]/a"));
         String AccDetails = AccountDetails.getText();
         Assert.assertTrue(AccDetails.contains("Account Details"), "La page Account Details n'existe pas");
     }
+
+
+
+   /* public void VerifAccountDetailsPage() {
+        WebElement AccountDetails = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[5]/a"));
+        String AccDetails = AccountDetails.getText();
+        Assert.assertTrue(AccDetails.contains("Account Details"), "La page Account Details n'existe pas");
+    }*/
 
     public void AccountDetailsPage() {
         WebElement AccountDetails = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/nav/ul/li[5]/a"));
@@ -237,7 +227,8 @@ public class MyAccountPage {
     public void verifBoutonSouscribe(){
         WebElement bouton = driver.findElement(By.xpath("//input[@type='submit']"));
         Assert.assertTrue(bouton.isDisplayed(), "Bouton non visible");
-    }
-*/
+    }*/
 
-}}
+
+}
+
