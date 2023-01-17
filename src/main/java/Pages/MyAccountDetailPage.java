3a38579c6965ceb30b41477e092b28f9c10ae121
salma <s.elatrach1@gmail.com>
2023-01-17 16:26:23 +0100

    tests de US5

--- src/main/java/Pages/MyAccountDetailPage.java
+++ src/main/java/Pages/MyAccountDetailPage.java
@@ -37,5 +37,44 @@ public class MyAccountDetailPage {
         WebElement champsMail = driver.findElement(By.id("account_email"));
         String Mail = champsMail.getAttribute("Softeam17@yopmail.com");
     }
+    public void SaisirMdpActuel( String mpactuel){
+        WebElement  mdpActuel = driver.findElement(By.id("password_current"));
+        mdpActuel.clear();
+        mdpActuel.sendKeys(mpactuel);
+}
+    public void SaisirNouveauMdp( String nouveaump){
+        WebElement   nouveauMdp= driver.findElement(By.id("password_1"));
+        nouveauMdp.sendKeys(nouveaump);
+    }
+
+    public void ConfirmerNouveauMdp( String nouveaump) {
+        WebElement confirmerMdp = driver.findElement(By.id("password_2"));
+        confirmerMdp.sendKeys(nouveaump);
+    }
+    public void EnregitrerModificationMps(){
+        WebElement enregistrermps = driver.findElement(By.name("save_account_details"));
+        enregistrermps.click();
+    }
+    public void Verifiermessage1(){
+        WebElement verefierMdp = driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/div[1]"));
+        verefierMdp.getText().contains("Account details changed successfully");
+    }
+    public void Verifiermessage2(){
+        WebElement verefierMdp2= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/ul/li"));
+        verefierMdp2.getText().contains("New passwords do not match.");
+    }
+
+    public void Verifiermessage3(){
+        WebElement verefierMdp3= driver.findElement(By.xpath("//*[@id=\"page-36\"]/div/div[1]/ul"));
+        verefierMdp3.getText().contains("New Your current password is incorrect.");
+    }
+
+
 
 }
+
+
+
+
+
+
--- src/test/java/Tests/T38SubscribeAccount.java
+++ src/test/java/Tests/T38SubscribeAccount.java
@@ -2,34 +2,29 @@ package Tests;
 
 import Pages.MyAccountPage;
 import Pages.Panier;
-
 import Pages.ShopPages;
+import com.google.common.eventbus.Subscribe;
 import commun.Hooks;
 import org.testng.annotations.Test;
 
-public class T39SubscribePanier extends Hooks {
-
+public class T38SubscribeAccount extends Hooks {
     String Val2 = "Softeam17@yopmail.com";
     String MPD ="Softeam@1";
 
 
     // VERIFIER la présence de boutton "subscribe here " dans page Panier
     @Test
-    public void testVerifierSubscrireIci (){
+    public void testVerifierSubscrireIci(){
         MyAccountPage accountConec = new MyAccountPage(driver);
-
-        ShopPages produit = new ShopPages(driver);
-
+        MyAccountPage subscribe = new MyAccountPage(driver);
+        Panier monpanier = new Panier(driver);
         accountConec.CliqueMyaccount();
         accountConec.SaisirmailCo(Val2);
         accountConec.SaisirMDPCo(MPD);
         accountConec.CliquerBoutonLogin();
-        produit.clickShop();
-        produit.clickAddBasket1();
-        produit.clickViewBasket();
-        Panier monpanier = new Panier(driver);
+        subscribe.CliqueMyaccount();
         monpanier.verifiechampssubscribehere("Softeam17@yopmail.com");
 
-    }
 
+    }
 }
